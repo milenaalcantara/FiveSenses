c40282c9f3650a0a60f9687809420788f35aada2
milenaalcantara <milena.lima.alcantara61@aluno.ifce.edu.br>
2023-06-06 11:43:09 -0300

    add whatchOS app

---
+++ FiveSenses-WatchOS Watch App/FiveSenses_WatchOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  FiveSenses_WatchOSApp.swift
+//  FiveSenses-WatchOS Watch App
+//
+//  Created by Milena Lima de Alcântara on 06/06/23.
+//
+
+import SwiftUI
+
+@main
+struct FiveSenses_WatchOS_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
