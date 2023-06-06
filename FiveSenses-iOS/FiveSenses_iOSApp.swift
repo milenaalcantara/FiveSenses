d4fd396a61824df05c926348acc4f1c50088a689
milenaalcantara <milena.lima.alcantara61@aluno.ifce.edu.br>
2023-06-06 11:40:03 -0300

    first commit

---
+++ FiveSenses-iOS/FiveSenses_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  FiveSenses_iOSApp.swift
+//  FiveSenses-iOS
+//
+//  Created by Milena Lima de Alcântara on 06/06/23.
+//
+
+import SwiftUI
+
+@main
+struct FiveSenses_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
